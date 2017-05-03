81942ae0e85e568ea1e49fdee5fbb0ed089c11d7
thierry.semon <semon@space.unibe.ch>
2017-05-03 22:17:13 +0200

    Code review

--- js/list-ctrl.js
+++ js/list-ctrl.js
@@ -6,6 +6,13 @@ function(AuthService, $http, $scope, $log, $state, AppService, $stateParams) {
     item.issuesFiltered = [];
     $scope.issuesFiltered = item.issuesFiltered; // point to issuesFiltered, required for $watch
 
+    $scope.$watch('issuesFiltered',
+    function() {
+        // callback function
+        console.log( item.issuesFiltered );
+    }, 
+    true);
+
     item.issuesFilterTexts = {
         buttonDefaultText: 'Filtre', 
         checkAll: 'Tout sélectionner', 
@@ -14,7 +21,7 @@ function(AuthService, $http, $scope, $log, $state, AppService, $stateParams) {
         searchPlaceholder: 'Rechercher...'};
 
     item.issuesFilter = [ 
-        {id: 1, label: "Nouveau", value: "new", option: "S"}, 
+        {id: 1, label: "Nouveau", value: "new", option: ""}, 
         {id: 2, label: "En cours", value: "inProgress", option: "S"}, 
         {id: 3, label: "Résolu", value: "resolved", option: "S"}, 
         {id: 4, label: "Rejeté", value: "rejected", option: "S"} ];
